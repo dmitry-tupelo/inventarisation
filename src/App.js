ec3e89c94f256947301e4bbeea65e44c9f236d3a
Dima M <54527086+dmitry-tupelo@users.noreply.github.com>
2023-04-22 16:36:53 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
 import HomePage from './pages/Homepage/Homepage';
 
-import { Fab } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
         <HomePage />
-      </header>
-      <div className="add-button">
-        <Fab color="primary" aria-label="add">
-              <AddIcon />
-          </Fab>
-      </div>
     </div>
   );
 }
